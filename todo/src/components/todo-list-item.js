b17ad0add525ed952c734cad08482696bed49a16
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2022-03-31 15:07:30 +0300

    components and props

---
+++ todo/src/components/todo-list-item.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoListItem = ( { label, important = false } ) => {
+
+  const liStyle = {
+    color: important ? 'tomato' : 'black'
+  }
+
+  return <span style={liStyle}>{ label }</span>
+}
+
+export default TodoListItem;
